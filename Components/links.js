2b6e2a5b2efe04559f53624e9cde655fcfd0fa0c
Hannah Ouazzane <hannahouazzane@gmail.com>
2021-10-29 18:11:00 +0100

    Add a styled nav bar to each page
    Co-authored-by: Ed Perkins <ed@edperkins.co.uk>

---
+++ Components/links.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+export const StyledLink = styled(Link)`
+  color: teal;
+  background-color: white;
+`;
+
+
+///not sure about this? Why does it not work
